70a97b59cc032c48241335ffee4b94671e9c308e
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-06 00:31:09 -0600

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,58 @@
+# --------- #
+# directory #
+# --------- #
+
+alias cdt="cd ~/Turing"
+alias ..="cd .."
+alias cl="clear"
+
+
+# --- #
+# git #
+# --- #
+
+alias gd="git diff"
+alias gs="git status"
+alias ga="git add"
+alias gc="git checkout"
+alias pm="git pull origin master"
+alias com="git checkout master"
+alias grv="git remote -v"                        # show remotes
+alias gbr="git browse"                           # open repo on github
+alias gcb="git checkout -b"                      # create and checkout to new branch
+alias glp="git log --pretty=oneline"             # oneline logs
+alias gl="git log --all --graph --decorate"      # detailed log
+
+# removes local branches that have been merged into master
+alias gclean="git branch --merged master | ag -v '\* master' | xargs -n 1 git branch -d"
+
+cobm() {
+  git checkout master && pull && git checkout -b "$1"
+}
+
+function gcm() {
+  args=$@
+  git commit -m "$args"
+}
+
+
+# ----- #
+# rails #
+# ----- #
+
+alias bi= "bundle install"
+alias test= "bundle exec rspec"
+alias be= "bundle exec"
+alias seed= "bundle exec rake db:reset"
+alias drop= "bundle exec rake db:drop"
+alias migrate= "bundle exec rake db:migrate"
+alias load = "bundle exec rake db:schema:load"
+alias server= "bundle exec rails s"
+
+
+# ------ #
+# heroku #
+# ------ #
+
+alias setupheroku= "heroku run rake db:setup"        # migrate & seed 
+
